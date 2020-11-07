3f2c3cfd8768dbdf184e606bfee694097953acec
Hack7654 <70554568+Hack7654@users.noreply.github.com>
2020-11-07 11:10:04 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,7 +13,8 @@ var bg = "sprites/bg1.png";
 var score = 0;
 
 function preload() {
-    getBackgroundImg();
+     bg = "sprites/bg1.png";
+    backgroundImg = loadImage(bg);
 }
 
 function setup(){
@@ -98,21 +99,4 @@ function keyPressed(){
        slingshot.attach(bird.body);
     }
 }
-
-async function getBackgroundImg(){
-    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
-    var responseJSON = await response.json();
-
-    var datetime = responseJSON.datetime;
-    var hour = datetime.slice(11,13);
-    
-    //if(hour>=0600 && hour<=1900){
-        bg = "sprites/bg1.png";
-    //}
-    //else{
-        //bg = "sprites/bg2.jpg";
-    //}
-
-    backgroundImg = loadImage(bg);
-    console.log(backgroundImg);
-}
+   
